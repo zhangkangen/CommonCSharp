a5ed6ca09554450979c0dc9cb7562b11bf5d2353
zhang.xiaokang <zhangkangen@gmail.com>
2016-01-12 14:40:01 +0800

    1、添加  Castle Windsor 2、面向接口编程

--- TestLog4net.MVC/Global.asax.cs
+++ TestLog4net.MVC/Global.asax.cs
@@ -1,21 +1,42 @@
-﻿using System;
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TestLog4net.MVC.Plumbing;
 
 namespace TestLog4net.MVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IWindsorContainer container;
+
+        private static void BootstrapContainer()
+        {
+            container = new WindsorContainer()
+                .Install(FromAssembly.This());
+            var controllerFactory = new WindsorControllerFactory(container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BootstrapContainer();
         }
+
+        protected void Application_End()
+        {
+            container.Dispose();
+        }
+
     }
 }
---
+++ TestLog4net.MVC/Installers/ControllerInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Castle.MicroKernel.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System.Web.Mvc;
+
+namespace TestLog4net.MVC.Installers
+{
+    public class ControllerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly()
+                .BasedOn<IController>()
+                .LifestyleTransient());
+        }
+    }
+}
