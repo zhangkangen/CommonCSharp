dc2d5edfec1f6c45e3240d64d07b4d7b25fb071f
zhang.xiaokang <zhangkangen@gmail.com>
2016-01-14 17:33:08 +0800

    添加model

---
+++ Platform.Model/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using Castle.ActiveRecord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Model
+{
+    public class BaseModel<T, TKey> : ActiveRecordBase<T>
+    {
+        public virtual TKey Id { get; set; }
+    }
+
+    public class BaseModel<T> where T : BaseModel<T, Int32>
+    {
+
+    }
+}
---
+++ Platform.Repository/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using Castle.ActiveRecord;
+using Castle.ActiveRecord.Framework;
+using Castle.ActiveRecord.Framework.Config;
+using Platform.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.Repository
+{
+    public class RepositoryBase
+    {
+        static RepositoryBase()
+        {
+            IConfigurationSource source = System.Configuration.ConfigurationManager.GetSection("activerecord") as IConfigurationSource;
+
+            ActiveRecordStarter.Initialize(source);
+
+        }
+    }
+}
