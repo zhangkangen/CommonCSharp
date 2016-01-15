f1dc05d9ff7e47ca85c24f9944509eb7d4b22b25
zhang.xiaokang <zhangkangen@gmail.com>
2016-01-15 15:24:51 +0800

    添加 easyui

--- Platform.Model/BaseModel.cs
+++ Platform.Model/BaseModel.cs
@@ -9,7 +9,7 @@ namespace Platform.Model
 {
     public class BaseModel<T, TKey> : ActiveRecordBase<T>
     {
-        public virtual TKey Id { get; set; }
+        public virtual TKey Id { get; set; } 
     }
 
     public class BaseModel<T> where T : BaseModel<T, Int32>
