488f9f6abb34c0859f9395b07b48120cd74c1be8
zhang.xiaokang <zhangkangen@gmail.com>
2016-02-25 10:00:02 +0800

    MachineKey加密生成

--- TestLog4net/Home.aspx.cs
+++ TestLog4net/Home.aspx.cs
@@ -2,11 +2,14 @@
 using Log4net.Common;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -19,12 +22,42 @@ namespace TestLog4net
         protected void Page_Load(object sender, EventArgs e)
         {
             _logger.Debug("home页面2");
+            Configuration config = WebConfigurationManager.OpenWebConfiguration("/");
+            MachineKeySection configSection = (MachineKeySection)config.GetSection("system.web/machineKey");
+            configSection.ValidationKey = CreateKey(64);
+            configSection.DecryptionKey = CreateKey(24);
+            configSection.Validation = MachineKeyValidation.SHA1;
+            configSection.Decryption = "3DES";
+            if (!configSection.SectionInformation.IsLocked)
+            {
+                config.Save();
+            }
         }
 
         public void test()
         {
             var a = 0;
             var i = 1 / a;
+        }
+
+        /// <summary>
+        /// 生成随机Key
+        /// </summary>
+        /// <param name="numBytes"></param>
+        /// <returns></returns>
+        public string CreateKey(int numBytes)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[numBytes];
+            rng.GetBytes(buff);
+            System.Text.StringBuilder hexString = new StringBuilder(64);
+            for (int i = 0; i < buff.Length; i++)
+            {
+                hexString.Append(String.Format("{0:X2}", buff[i]));
+            }
+
+            return hexString.ToString();
+
         }
     }
 }
